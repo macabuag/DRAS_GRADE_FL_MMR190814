697d1b87c446467c42ca7dbf67aee71b049ad53f
macabuag <macabuag@gmail.com>
2019-08-15 22:06:56 +0100

    read_census.r successfully reads and combines the Baseline_Cencus tables

--- scripts/read_census.r
+++ scripts/read_census.r
@@ -24,9 +24,48 @@ if(!require(readxl)) {
   library(readxl)
 }
 
+if(!require(stringr)) {
+  install.packages("stringr")
+  library(stringr)
+}
+
 ## 2.0 READ SPREADSHEETS ---------------------------------------------------
 
-a <- data.table(read_excel(path = file.path(iFile$censusFolder, "BaselineData_Census_Ayeyarwady_with_Pcode_MIMU_05Jun2015_Eng.xlsx"),
-                sheet = "Table I-1", skip = 3, n_max = 100))
-b <- a[!is.na(`Township Pcode`)]
-if (anyDuplicated(b$`Township Pcode`) != 0) warning("'Township Pcode not unique")
+iFile$censusList <- list.files(path = iFile$censusFolder, pattern = "*_Eng*")
+
+Census <- list()
+
+
+for(iCensus in iFile$censusList) {
+  # a <- data.table(read_excel(path = file.path(iFile$censusFolder, "BaselineData_Census_Ayeyarwady_with_Pcode_MIMU_05Jun2015_Eng.xlsx"),
+  #                 sheet = "Table I-1", skip = 3, n_max = 200))
+  iState <- word(string = iCensus, start = 3, sep = "_")
+  
+  if (iState %in% c("Union")) next #don't read Union (it isn't a state, but an amalgamated table of all states)
+  
+  a <- data.table(read_excel(path = file.path(iFile$censusFolder, iCensus),
+                             sheet = "Table I-1", skip = 3, n_max = 200))
+
+  if (!"Township Pcode" %in% names(a)) {
+    a <- data.table(read_excel(path = file.path(iFile$censusFolder, iCensus),
+                               sheet = "Table I-1", skip = 4, n_max = 200))
+  } #sometimes the table starts from the 4th sometimes the 5th row
+    
+  b <- a[!is.na(`Township Pcode`)]
+  if (anyDuplicated(b$`Township Pcode`) != 0) warning("'Township Pcode not unique")
+  Census$state$Tab_I[[iState]] <- b
+  rm(a)
+}
+rm(b)
+
+c <- rbindlist(Census$state$Tab_I, use.names = T, fill = T)
+if (anyDuplicated(c$`Township Pcode`) != 0) warning("'Township Pcode not unique")
+
+
+Census$Combined <- c
+rm(c)
+
+## 3.0 WRITE OUTPUTS ----------------------------------------------------------------------------------
+dir.create("outputs", showWarnings = F)
+fwrite(Census$Combined, file = file.path("outputs", "BaselineData_Census_COMBINED_with_Pcode_MIMU_05Jun2015_Eng.csv"))
+
